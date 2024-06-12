a097181013382a051456c61093da20386b1af229
dox1234dh <dox1dh@gmail.com>
2024-06-12 10:06:31 -0400

    week3: django admin car make , car model

--- server/djangoapp/admin.py
+++ server/djangoapp/admin.py
@@ -1,12 +1,17 @@
-# from django.contrib import admin
-# from .models import related models
+from django.contrib import admin
+from .models import CarMake, CarModel
+
+
 
 
 # Register your models here.
 
+# Registering models with their respective admins
 # CarModelInline class
-
+admin.site.register(CarMake)
 # CarModelAdmin class
+admin.site.register(CarModel)
+
 
 # CarMakeAdmin class with CarModelInline
 
